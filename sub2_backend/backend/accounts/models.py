a3f2b29983b74eef708d6e41931d08c562f21f42
yeafla530 <yeafla530@naver.com>
2020-09-24 18:34:24 +0900

    test backend

--- sub2_backend/backend/accounts/models.py
+++ sub2_backend/backend/accounts/models.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 # Create your models here.
 #확장성 보장
 class User(AbstractUser):
-    pass
+    first_name = None
+    last_name = None
+    username = None
+    email = models.EmailField('이메일', unique=True)
+    password = models.CharField('비밀번호', max_length=128)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_RIELDS = []
+
+    objects = UserManager()
